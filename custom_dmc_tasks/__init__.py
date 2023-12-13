dea4f9e38e8adb66890c79bf308e84a06f5b7b14
Daniel Khapun <daniel.khapun@gmail.com>
2023-12-13 14:17:01 +0200

    added urlb tasks

--- custom_dmc_tasks/__init__.py
+++ custom_dmc_tasks/__init__.py
@@ -1,4 +1,6 @@
+from custom_dmc_tasks import cheetah
 from custom_dmc_tasks import walker
+from custom_dmc_tasks import hopper
 from custom_dmc_tasks import quadruped
 from custom_dmc_tasks import jaco
 
@@ -8,7 +10,12 @@ def make(domain, task,
          environment_kwargs=None,
          visualize_reward=False):
     
-    if domain == 'walker':
+    if domain == 'cheetah':
+        return cheetah.make(task,
+                            task_kwargs=task_kwargs,
+                            environment_kwargs=environment_kwargs,
+                            visualize_reward=visualize_reward)
+    elif domain == 'walker':
         return walker.make(task,
                            task_kwargs=task_kwargs,
                            environment_kwargs=environment_kwargs,
